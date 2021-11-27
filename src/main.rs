52e5a0a6c6af453811bdada713af8dc20ad51e14
John Arumemi-Ikhide <john.arumemi@gmail.com>
2021-11-27 19:34:34 +0000

    fix: incorrect prelude in root module

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod prelude { // use 'mod' to create a new module within the source code
     pub use bracket_lib::prelude::*; // re-export it
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
-    pub use crate::map;  // use 'crate::' to reference the current module scope and re-export map with 'pub'
+    pub use crate::map::*;  // use 'crate::' to reference the current module scope and re-export map with 'pub'
 }
 
 // use our created prelude within this module's main scope
