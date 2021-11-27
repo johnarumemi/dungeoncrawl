bc0cad22a4a06270c373b1d6247a2def49c9ad04
John Arumemi-Ikhide <john.arumemi@gmail.com>
2021-11-27 19:30:17 +0000

    add TileType

--- src/map.rs
+++ src/map.rs
@@ -1,3 +1,14 @@
 use crate::prelude::*;
 
 const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+// tiles are limited to a pre-defined set of tile types
+// Clone enable deep copy
+// Copy changes default behaviour when assigning a TileType from one variable to another
+// PartialEq allows us to compare TileType values with == operator
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor
+}
+
